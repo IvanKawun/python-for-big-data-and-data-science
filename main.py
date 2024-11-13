1910a26c5f18e1d7f84d24fccff7b641e5ac0127
123 <van.moshk@gmail.com>
2024-11-13 23:59:02 +0300

    Ratings data frame edit function
    transform titles from camel to snake case,  corrected datatypes and contril missing values by making them 0

--- main.py
+++ main.py
@@ -15,13 +15,16 @@ from pyspark.sql import SparkSession
 from io1 import read_ratings_df, write_ratings_df_to_csv
 from io1 import RATINGS_FILE_PATH
 from io1 import RESULTS_PACKAGE_PATH
+from title_ratings_editing import transform_title_ratings
 
 spark_session = (SparkSession.builder
-                                 .master("local")
-                                 .appName("ratings_dfs")
-                                 .getOrCreate()
-                     )
-df = read_ratings_df(RATINGS_FILE_PATH, spark_session)
-df.show()
-write_ratings_df_to_csv(df, RESULTS_PACKAGE_PATH)
+                 .master("local")
+                 .appName("ratings_dfs")
+                 .getOrCreate()
+                )
+
+df_ratings = read_ratings_df(RATINGS_FILE_PATH, spark_session)
+df_transformed = transform_title_ratings(df_ratings)
+df_transformed.show()
+write_ratings_df_to_csv(df_transformed, RESULTS_PACKAGE_PATH)
 
