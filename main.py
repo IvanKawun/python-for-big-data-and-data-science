ba2611dcff29dd70b2e41ac206d82bfc7f5ad1fe
123 <van.moshk@gmail.com>
2024-11-16 03:22:28 +0300

    ...

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ view_dataframe()
 
 import pyspark.sql.types as t
 from pyspark.sql import SparkSession
-from pyspark.sql.pandas.types import from_arrow_type
 
 from io1 import read_ratings_df, write_ratings_df_to_csv
 from io1 import RATINGS_FILE_PATH
@@ -30,37 +29,38 @@ spark_session = (SparkSession.builder
                  .appName("ratings_dfs")
                  .getOrCreate()
                 )
-"""
+
 df_ratings = read_ratings_df(RATINGS_FILE_PATH, spark_session)
 df_transformed = transform_title_ratings(df_ratings)
-write_ratings_df_to_csv(df_transformed, RESULTS_PACKAGE_PATH)
+"""write_ratings_df_to_csv(df_transformed, RESULTS_PACKAGE_PATH)"""
 
 df_crew = spark_session.read.option("delimiter", "\t").csv("data/title.crew.tsv.gz", header=True)
 df_crew_transformed = transform_title_crew(df_crew)
-write_ratings_df_to_csv(df_crew_transformed, RESULTS_PACKAGE_PATH)
-"""
-"""
+"""write_ratings_df_to_csv(df_crew_transformed, RESULTS_PACKAGE_PATH)"""
+df_crew_transformed.show()
+
 df_principals = spark_session.read.option("delimiter", "\t").csv("data/title.principals.tsv.gz", header=True)
 df_principals_transformed = transform_title_principals(df_principals)
-write_ratings_df_to_csv(df_principals_transformed, RESULTS_PACKAGE_PATH)
-"""
-"""
+"""write_ratings_df_to_csv(df_principals_transformed, RESULTS_PACKAGE_PATH)"""
+df_principals_transformed.show()
+
+
 df_basics = spark_session.read.option("delimiter", "\t").csv("data/name.basics.tsv.gz", header=True)
 df_basics_transformed = transform_name_basics(df_basics)
-write_ratings_df_to_csv(df_basics_transformed, RESULTS_PACKAGE_PATH)
-"""
-"""
+"""write_ratings_df_to_csv(df_basics_transformed, RESULTS_PACKAGE_PATH)"""
+df_basics_transformed.show()
+
 df_akas = spark_session.read.option("delimiter", "\t").csv("data/title.akas.tsv.gz", header=True)
 df_akas_transformed = transform_title_akas(df_akas)
-write_ratings_df_to_csv(df_akas_transformed, RESULTS_PACKAGE_PATH)
-"""
-"""
+"""write_ratings_df_to_csv(df_akas_transformed, RESULTS_PACKAGE_PATH)"""
+df_akas_transformed.show()
+
 df_episode = spark_session.read.option("delimiter", "\t").csv("data/title.episode.tsv.gz", header=True)
 df_episode_transformed = transform_title_episode(df_episode)
-write_ratings_df_to_csv(df_episode_transformed, RESULTS_PACKAGE_PATH)
-"""
-"""
+"""write_ratings_df_to_csv(df_episode_transformed, RESULTS_PACKAGE_PATH)"""
+df_episode_transformed.show()
+
 df_title_basics = spark_session.read.option("delimiter", "\t").csv("data/title.basics.tsv.gz", header=True)
-df_episode_transformed = transform_title_basics(df_title_basics)
-write_ratings_df_to_csv(df_episode_transformed, RESULTS_PACKAGE_PATH)
-"""
+df_title_basics_transformed = transform_title_basics(df_title_basics)
+"""write_ratings_df_to_csv(df_title_basics_transformed, RESULTS_PACKAGE_PATH)"""
+df_title_basics_transformed.show()
